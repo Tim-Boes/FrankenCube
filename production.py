5f14f71a482384592caf548b799f77dd57b32238
Tim-Fabian Boes <boes@ph1.uni-koeln.de>
2024-05-07 13:36:24 +0200

    shifty array production file

--- production.py
+++ production.py
@@ -1,44 +1,40 @@
 import torch
 import numpy as np
-import lightning
+import torch.multiprocessing as mp
 from models.convolutional_autoencoder import ConvolutionalAutoencoder
 from data.hdf5_subcube_dataset import SubcubeDataset
-import matplotlib.pyplot as plt
-
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-
-model = ConvolutionalAutoencoder.load_from_checkpoint(
-    '/home/ace/Documents/CODE/TIM_REPO/FrankenCube/frankencube/mn4ucyrn/checkpoints/epoch=6-step=114688.ckpt'
-)
-
-dataset = SubcubeDataset(
-    data_directories=['/media/ace/Warehouse/prp_files'],
-    extension='.hdf5',
-    sc_side_length=16,
-    stride=16,
-    physical_paramters=['dens' , 'temp'],
-)
 
 
+def scatter_plott(point_list):
+    index_range = point_list[0]
+    model = point_list[1]
+    dataset = point_list[2]
+    points = []
+    for i in range(index_range[0], index_range[1]):
+        points.append(
+            model.encode(torch.from_numpy(dataset[i]["data"]))[0].detach().numpy()
+        )
+    np.save(f"./scatterpoints_{i}", points)
 
 
-
-cm = plt.cm.get_cmap('RdYlBu')
-
-
-for i in range(len(dataset)):
-    print(len(dataset)-i)
-    point = model.encode(torch.from_numpy(dataset
-        [i]['data']))[0].detach().numpy()
-    sc = plt.scatter(
-        point[0],
-        point[1],
-        c=i,
-        cmap=cm
+if __name__ == "__main__":
+    model = ConvolutionalAutoencoder.load_from_checkpoint(
+        "/home/ace/Documents/CODE/TIM_REPO/FrankenCube/frankencube/mn4ucyrn/checkpoints/epoch=6-step=114688.ckpt"
     )
-    
-    # plt.pause(0.05)
-plt.colorbar(sc)
-plt.savefig('./Scatter_plot', format='png')
 
+    dataset = SubcubeDataset(
+        data_directories=["/media/ace/Warehouse/prp_files"],
+        extension=".hdf5",
+        sc_side_length=16,
+        stride=16,
+        physical_paramters=["dens", "temp"],
+    )
+    scatter_points = []
+    point_list = [
+        [[0, 500], model, dataset],
+        [[501, 1000], model, dataset],
+        [[1001, 1500], model, dataset],
+    ]
+    mp.set_start_method("fork")
+    with mp.Pool(3) as p:
+        p.map(func=scatter_plott, iterable=point_list)
