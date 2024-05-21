50c243c7d2d8b62290e52e8d89c0cfa65f3e81ec
Tim-Boes <boes@ph1.uni-koeln.de>
2024-05-22 00:49:23 +0200

    change init

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from .convolutional_autoencoder import ConvolutionalAutoencoder
+from .convolutional_autoencoder_high_res import ConvolutionalAutoencoderHighRes
 
 __all__ = [
     "ConvolutionalAutoencoder",
