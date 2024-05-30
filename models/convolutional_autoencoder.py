720ad987598212272d65d65c2ae44eec4da90225
Tim-Boes <boes@ph1.uni-koeln.de>
2024-05-30 23:50:50 +0200

    add sc64 conv settings

--- models/convolutional_autoencoder.py
+++ models/convolutional_autoencoder.py
@@ -274,10 +274,15 @@ class ConvolutionalAutoencoderSC64(L.LightningModule):
 
         # to find the output use:
         # N = (input + 2 * padding - kernel) * (1/stride) + 1
-        # input subcube 2x32x32x32
+        # input subcube 2x64x64x64
 
+        self.conv1 = nn.Conv3d(
+            in_channels=2, out_channels=4,
+            kernel_size=(3,3,3), stride=1, padding=1)  # 4x64x64x64
+        self.pool1 = nn.MaxPool3d(
+            kernel_size=(2, 2, 2), stride=2, padding=0)  # 4x32x32x32
         self.conv2 = nn.Conv3d(
-            in_channels=2, out_channels=8,
+            in_channels=4, out_channels=8,
             kernel_size=(3, 3, 3), stride=1, padding=1)  # 8x32x32x32
         self.pool2 = nn.MaxPool3d(
             kernel_size=(2, 2, 2), stride=2, padding=0)  # 8x16x16x16
@@ -323,11 +328,13 @@ class ConvolutionalAutoencoderSC64(L.LightningModule):
             kernel_size=(4, 4, 4), stride=2, padding=1)  # 4x32x32x32
         self.deconv5 = nn.ConvTranspose3d(
             in_channels=4, out_channels=2,
-            kernel_size=(3, 3, 3), stride=1, padding=1)  # 2x32x32x32
+            kernel_size=(4, 4, 4), stride=2, padding=1)  # 2x64x64x64
 
     def encode(self, x):
         """Encode into tensor
         """
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
         x = F.relu(self.conv2(x))
         x = self.pool2(x)
         x = F.relu(self.conv3(x))
